54a3842ac3c572422d8729f16ab2043ec7108009
Christopher Fordant <christopherfordant1@gmail.com>
2026-03-24 11:44:14 +0100

    Refine coded home header sports hover behavior

--- prototype-site/app.js
+++ prototype-site/app.js
@@ -2,7 +2,6 @@ const sportButtons = document.querySelectorAll(".sport-link");
 
 sportButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    sportButtons.forEach((item) => item.classList.remove("is-active"));
-    button.classList.add("is-active");
+    button.blur();
   });
 });
